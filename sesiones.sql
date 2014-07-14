ed65f0650b383cf349ea44a66c8088e01ff65918
Daniel Berrocal <jdanielb1992@gmail.com>
2014-07-14 13:48:49 -0600

    script

---
+++ sesiones.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-07-2014 a las 19:52:27
+-- Versión del servidor: 5.6.16
+-- Versión de PHP: 5.5.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `sesiones`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pregunta`
+--
+
+CREATE TABLE IF NOT EXISTS `pregunta` (
+  `id_pregunta` int(11) NOT NULL AUTO_INCREMENT,
+  `enunciado` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_pregunta`),
+  UNIQUE KEY `id_pregunta` (`id_pregunta`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `pregunta`
+--
+
+INSERT INTO `pregunta` (`id_pregunta`, `enunciado`) VALUES
+(1, 'Nombre de la Madre'),
+(2, 'Nombre del primer perrito');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `usuario` varchar(20) NOT NULL,
+  `clave` varchar(10) NOT NULL,
+  `pregunta` int(11) NOT NULL,
+  `respuesta` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `id_usuario` (`id_usuario`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nombre`, `usuario`, `clave`, `pregunta`, `respuesta`) VALUES
+(1, 'jorge', 'jeragones', '123', 1, 'pecas'),
+(2, 'jorge', 'jeragones', '123', 1, 'pecas'),
+(3, 'Nixon', 'metalslave', 'nxn119313', 1, 'Mairene');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
